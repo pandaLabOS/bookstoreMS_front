9af8e39399de5d77159e67b4d8d4750ba6daefc2
pandaLabOS <u6215112@au.edu>
2023-03-22 20:42:28 +0700

    Table component and form for adding new data
    Co-authored-by: MrKTPPPP <MrKTPPPP@users.noreply.github.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from '/components/Navbar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header>
+        <NavBar/>
+      </header>
+      <Component {...pageProps} />
+      
+    </>
+  )
 }
